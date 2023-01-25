8b2db6e0c345eade53800442f0a95b36a8f029f2
ttornikes <silakadzetornike14@gmail.com>
2023-01-26 01:42:41 +0400

    backend-routerControler

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import express  from "express";
-
+import postRouter from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
 const app =express()
 
 app.use(express.json())
-
+app.use("/api/posts",postRouter)
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
 
 app.listen(8800,()=>{
     console.log("conected!")
